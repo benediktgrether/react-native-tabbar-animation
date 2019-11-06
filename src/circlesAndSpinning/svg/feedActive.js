9592de29b028b11b7a38f9dfa13a03d1e3d3dce8
Benedikt Grether <info@benediktgrether.de>
2019-11-06 19:38:29 +0100

    Edit Fill and Stroke color

--- src/circlesAndSpinning/svg/feedActive.js
+++ src/circlesAndSpinning/svg/feedActive.js
@@ -2,15 +2,16 @@ import React, { Component } from "react";
 import { TouchableWithoutFeedback, Animated, Easing } from "react-native";
 import { Svg, Path } from "react-native-svg";
 
-const AnimatedPath = Animated.createAnimatedComponent(Path);
-
 export default class FeedActiveIcon extends Component {
   constructor() {
     super();
     this.state = {
       rotateY: 0,
-      fill: "none"
+      fill: "none",
+      stroke: "#4e4e50"
     };
+
+    const colorValue = "#25bfa9";
 
     let fillColor = false;
     this.offsetValue = new Animated.Value(0);
@@ -19,27 +20,18 @@ export default class FeedActiveIcon extends Component {
     this.rotateYValue.addListener(rotateY => {
       this.setState({ rotateY: rotateY.value });
       if (rotateY.value >= 0.5 && fillColor == false) {
+        // fill Color on True to get the change only one Time
         fillColor = true;
-        console.log("test");
-        this.state.fill = "#25bfa9";
-        console.log(this.state.fill);
+        // this.state.fill to fill the Icon
+        this.state.fill = colorValue;
+        // this.state.stroke get the new outline color
+        this.state.stroke = "none";
       }
     });
   }
 
   componentDidMount() {
-    // this.offset();
     this.rotation();
-  }
-
-  offset() {
-    this.offsetValue.setValue(0);
-    Animated.timing(this.offsetValue, {
-      toValue: 1,
-      duration: 1000,
-      easing: Easing.inOut(Easing.quad),
-      useNativeDriver: true
-    }).start();
   }
 
   rotation() {
@@ -52,39 +44,21 @@ export default class FeedActiveIcon extends Component {
   }
 
   render() {
-    const offset = this.offsetValue.interpolate({
-      inputRange: [0, 1],
-      outputRange: [150, 0]
-    });
-
     const rotateY = this.rotateYValue.interpolate({
       inputRange: [0, 1],
       outputRange: ["0deg", "180deg"]
     });
-    const { fill } = this.state;
+    const { fill, stroke } = this.state;
     return (
       <Animated.View style={{ transform: [{ rotateY }] }}>
         <Svg width="31.64" height="29.69" viewBox="0 0 31.64 29.69">
           <Path
             d="M30.32,16.64v9.72a2,2,0,0,1-2,2H20.57a2,2,0,0,1-2-2V20.5a2.61,2.61,0,1,0-5.21,0v5.86a2,2,0,0,1-2,2H3.65a2,2,0,0,1-2-2V16.64a3.3,3.3,0,0,1,1-2.33L14.7,2.19A1.83,1.83,0,0,1,16,1.65a1.79,1.79,0,0,1,1.29.54L29.36,14.31A3.3,3.3,0,0,1,30.32,16.64Z"
             transform="translate(-0.18 -0.15)"
-            fill="none"
-            stroke="#4e4e50"
-            strokeWidth="3"
-          />
-          <AnimatedPath
-            d="M30.32,16.64v9.72a2,2,0,0,1-2,2H20.57a2,2,0,0,1-2-2V20.5a2.61,2.61,0,1,0-5.21,0v5.86a2,2,0,0,1-2,2H3.65a2,2,0,0,1-2-2V16.64a3.3,3.3,0,0,1,1-2.33L14.7,2.19A1.83,1.83,0,0,1,16,1.65a1.79,1.79,0,0,1,1.29.54L29.36,14.31A3.3,3.3,0,0,1,30.32,16.64Z"
-            transform="translate(-0.18 -0.15)"
             fill={fill}
-            stroke="#25bfa9"
-            strokeDasharray="150"
-            strokeDashoffset={offset}
-            // stroke-miterlimit="10"
+            stroke={stroke}
             strokeWidth="3"
           />
-
-          {/* Starting Animation Rendering when it becames Active State */}
-          {/* {this.offset()} */}
         </Svg>
       </Animated.View>
     );
