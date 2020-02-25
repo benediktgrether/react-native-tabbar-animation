b5f0035906251d495add740b6b64b777b312eb32
Benedikt Grether <info@benediktgrether.de>
2020-02-25 12:36:59 +0100

    Add Web-Build

---
+++ ii-Tab-Bar-Animation/03-Bottom-Slide-Animation-Animation03/web-build/precache-manifest.737b34d615f72547a5baeaab20cc6b6a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "233adaf9232ff475a51f",
+    "url": "/static/js/app.7eace41e.chunk.js"
+  },
+  {
+    "revision": "8316563a40f3e5b2ed79",
+    "url": "/static/js/runtime~app.b178e826.js"
+  },
+  {
+    "revision": "34481866b3b15b7a3c63",
+    "url": "/static/js/2.f943b668.chunk.js"
+  },
+  {
+    "revision": "6165c9d7a2e729ba57b23dd93add5366",
+    "url": "/static/media/back-icon-mask.6165c9d7.png"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "5e695e96a003a79f7f97060bf49409a9",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "af476d73a81297d563dc5ba31333ba31",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4209c8e45247374ad12a7ca6b1667574",
+    "url": "/index.html"
+  }
+];
